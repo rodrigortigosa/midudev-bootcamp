b408b4c0e3a55fce648b1923bde8dbefeac087bb
rodrigortigosa <rodrigortigosa@gmail.com>
2021-02-09 16:59:21 -0300

    Mi primer app en React

---
+++ Part1/aprendiendo-react/primer-app-react/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Mensaje from "./Mensaje.js";
+
+const Descripcion = () => {
+  return <p>Esta es la app del curso fullstack bootcamp</p>;
+};
+
+const App = () => {
+  const titulo = 'Mi primer app en React';
+
+  return (
+    <>
+      <h1>{titulo}</h1>
+      
+      <Mensaje message='Estamos trabajando' color='red' />
+      <Mensaje message='en un curso' color='green' />
+      <Mensaje message='de React' color='blue'></Mensaje>
+      <Descripcion></Descripcion>
+    </>
+  );
+};
+
+export default App;
